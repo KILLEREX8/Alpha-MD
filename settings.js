5ab516505efde9cc9d7ffa076a62340364b1e9a7
KILLEREX8 <gamingkillerex@gmail.com>
2025-06-01 19:47:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Alpha_MD",
+  botOwner: 'Sathira Bot', // Your name
+  ownerNumber: '94726455848', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
